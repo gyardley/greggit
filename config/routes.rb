2a7ee8b6b6c45c7e2cfc0cb3b8d05a72bcd1592b
Greg Yardley <greg@yardley.ca>
2014-03-06 18:52:06 -0300

    Fixed the damn rspec / capybara tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Greggit::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
 
+  devise_scope :user do
+    get 'sign_in', :to => 'devise/sessions#new', :as => :new_user_session
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   root 'static#index'
 
 end
--- spec/features/login_tests_spec.rb
+++ spec/features/login_tests_spec.rb
@@ -1,22 +1,23 @@
 require 'spec_helper'
 
 describe "integration tests for login" do
+
   it "should show a link to Twitter when not logged in " do
-      visit root_path
-      page.html.should match /Sign in with Twitter/
+    visit root_path
+    expect(page).to have_content "Sign in with Twitter"
   end
 
   it "should correctly log in a user via twitter" do
     visit root_path
     click_link('Sign in with Twitter')
-    page.html.should match /Signed in./
+    expect(page).to have_content "Signed in."
   end
 
   it "should correctly log out a user" do
     visit root_path
     click_link('Sign in with Twitter')
-    page.html.should match /Signed in./
+    expect(page).to have_content "Signed in."
     click_link('Sign out')
-    page.html.should match /Sign in with Twitter/
+    expect(page).to have_content "Sign in with Twitter"
   end
 end
