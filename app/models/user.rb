bdb4918ed51ac0cecdd85756b0aa44c20f7f5f01
Greg Yardley <greg@yardley.ca>
2014-03-06 17:59:16 -0300

    Views and controllers for twitter oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,20 @@ class User < ActiveRecord::Base
 
   devise :omniauthable
 
+  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+
+    if user
+      return user
+    else
+      registered_user = User.where(uid: auth.uid).first
+      if registered_user
+        return registered_user
+      else
+        user = User.create(provider:auth.provider, uid:auth.uid)
+      end
+    end
+  end
+
 end
