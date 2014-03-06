8795085e61fa26a189696c680d603e71f0beb2dc
Greg Yardley <greg@yardley.ca>
2014-03-06 17:34:09 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+ruby '2.1.0'
+
+# Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
+gem 'rails', '4.0.3'
+
+gem 'bcrypt-ruby', '~> 3.1.2'
+gem 'bootstrap-sass', '~> 3.1.1.0'
+gem 'devise', '~> 3.2.3'
+gem 'figaro', '~> 0.7.0'
+gem 'jquery-rails', '~> 3.0.4'
+gem 'omniauth-twitter', '~> 1.0.1'
+gem 'pg', '~> 0.17.0'
+gem 'turbolinks'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+
+group :production do
+  gem "rails_12factor", '~> 0.0.2' # Heroku Rails 4 requirement
+end
+
+group :test do
+  gem 'rspec', '~> 2.14.1'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Greggit::Application.config.secret_key_base = '4dafb4f2576f79989c9aafd3c465d6918a29566fab863c0f84b986c3532f5b32131f57e997ffc1222f3290be870d401f06aff1fe4c2bcaf8212e19c956a6e72d'
